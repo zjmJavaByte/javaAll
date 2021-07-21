10c7429af62db496f0a877c1129300ec9073bd21
laoyouji1018 <17734808250@163.com>
2021-07-21 12:21:15 +0800

    初始化

---
+++ src/test/java/com/qaaq/java/JavaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.qaaq.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("123");
+    }
+
+}
